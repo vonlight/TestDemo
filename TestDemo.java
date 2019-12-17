185f80d4859468e77865b1b5f4da2d7ef7f21479
vonlight <56585824+vonlight@users.noreply.github.com>
2019-12-17 20:20:22 +0800

    ABAND
    阿姆斯特朗

--- TestDemo.java
+++ TestDemo.java
@@ -1,4 +1,9 @@
 public class TestDemo {
+  private static class Test {
+    public void print() {
+      System.out.println("你好世界");
+    }
+  }
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
